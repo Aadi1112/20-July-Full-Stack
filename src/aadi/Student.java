d2028fab8c1e9a2d8d31d540c3c833a056e84c6b
aadianth <aadi2000@gmail.com>
2025-07-24 06:59:48 +0530

    datatype and typecasting

---
+++ src/aadi/Student.java
@@ -0,0 +1,20 @@
+package aadi;
+
+class Student 
+{
+    //main Method
+	
+	//int main(){}
+	//int age=23;
+	static public void main(String[]args)
+	{
+		//printf()
+		//cout<<
+		
+		System.out.println("Hello I am aadi");
+		
+          
+		
+	}
+	
+}
---
+++ src/hari/Demo.java
@@ -0,0 +1,151 @@
+package hari;
+
+public class Demo {
+	
+	char x; //single space
+
+	boolean b;//false
+	
+	String s; //null
+	
+	Demo d;
+	public static void main(String[] args) {
+
+		//Demo d=new Demo();
+	//classname ref.var = new Classname();
+		
+		//System.out.println(d.d); //0
+		// Variable - >
+		// Variable is like a contianer
+		// which is used to store data on computer memeory
+
+		// Variable Declaration
+		// datatype variableName;
+		// int age;
+		// Variable initialization
+		// variableName = value ;
+
+		// age = 23;
+
+		// declaration + initialzation
+		// int id=23;
+
+//		   int a,b,c; 
+//		   
+//		   a=12;b=45;c=45;
+//		   
+//		   System.out.println(a+b+c);
+//		   
+//		   int id=233,age=233,marks=32;
+//		   
+//		System.out.println(age);
+//		System.out.println(10*2*3);
+
+		// Painful ---- Beautiuful
+		
+//		String name="Aadinath"; //16byte
+//		
+//		byte age = 127;
+//
+//		byte marks = 98;
+//
+//		byte b = 11; // RAM - 1byte storage +127 - 128
+//
+//		short s = 22222; // 2Byte 32767 -32768
+//
+//		int i = 333333333; // 4 byte
+//
+//		long l = 1111111111111111111L; // 8 BYTE
+//
+//		float f = 400.23f; // 4 byte
+//		System.out.println(f);
+//
+//		double salary = 30000.79; // 8 byte
+//		System.out.println(salary);
+//
+//		char gender = 'F'; // 2 bytes
+//		System.out.println(gender);
+//
+//		boolean isMarried = false; // 1 bit 8 bit - 1 byte
+//
+//		System.out.println(isMarried);
+
+		// Rules to define variable
+
+		// 1.Variable name should be start with character and $, _
+		// ex.int age
+		// int 1abc; @abc wrong
+		// 2.Variable name can not contain space...
+		// ex.int student marks wrong int studentMarksOfBcaStudent - correct
+		// 3.Variable name can contain dollar($) and underscore(_) as a special char
+		// only
+//		int student_Marks;
+//
+//		int _abc;
+//
+//		int a1bc2;
+//
+//		int a;
+//
+//		int aa = 11;
+//		//System.out.println(aa);
+//		char c = '2';
+
+//Ctrl + shift +f - to format your code
+		
+//		byte b=100;
+//		
+//		short s=b;
+//		
+//		int i=s;
+//		
+//		long l=i;
+//		
+//		float f=l;
+//		
+//		double d=f;
+//		System.out.println(d);
+		
+		
+		
+		double salary=100;
+		
+		float f=(float)salary;
+		
+		long l=(long)f;
+		
+		int i=(int)l;
+		
+		short s=(short)i;
+		
+		byte b=(byte)i;
+		
+		System.out.println(f);
+		System.out.println(l);
+		System.out.println(i);
+		System.out.println(s);
+		System.out.println(b);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+}
