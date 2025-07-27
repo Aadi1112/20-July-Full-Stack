b1c74192bc028d2a9043a032edfa2091bcdd27eb
Aadinath Kagade <125958247+Aadi1112@users.noreply.github.com>
2025-07-27 19:12:56 +0530

    Scanner Class

---
+++ ScannerClass/src/aadiandjava/Demo.java
@@ -0,0 +1,56 @@
+package aadiandjava;
+import java.util.Scanner;
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		Scanner ee = new Scanner(System.in); //Object of Scanner class
+		
+		//We are creating object of Scanner Class to access Method from SCanner class to take the inpur from user
+		
+//		System.out.println("Enter your Gender (M- Male , F-Female)");
+//		
+//		char gender;
+//		
+//		gender=ee.next().charAt(0);
+//		
+//		System.out.println("Your gender is : "+gender);
+		
+		
+		
+		//predefined Method - Scanner 
+		
+		//String - next()
+		//int   - nextInt()
+		//float - nextFloat()
+		//boolean - nextBoolean()
+		//byte - nextByte()
+		//short- nextShort()
+		//long - nextLong()
+		//char ->next().charAt(0);
+		
+		
+		
+//		String name;
+//		System.out.println("Enter Your Name");
+//		name=ee.next();//it is used to read String value from console
+//		
+//		System.out.println("Your Name is : "+name);
+		
+//		float height;
+//		System.out.println("Enter Your Hieght");
+//		
+//		height=ee.nextFloat();//it is used to read float value from console
+//		
+//		System.out.println("Your Height is :"+height);
+		
+		
+//		int age;
+//		System.out.println("Enter Your Age...");
+//		
+//		age=ee.nextInt();//It is used read integer value from console
+//		
+//		System.out.println("Entered Age : "+age);
+		
+	}
+}
