56aba91a5b47fb6e3f453468505ba82b0cf2ca8c
Aadinath Kagade <125958247+Aadi1112@users.noreply.github.com>
2025-07-30 07:37:04 +0530

    Add files via upload
    Nested For Loop , Pattern 1

---
+++ looping/Demo.java
@@ -0,0 +1,201 @@
+package com.looping;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		//2 Times 
+		//OOPS , Exception Handling , Collection = Fundamental
+		          //11<=10
+//		for(int i=1;i<=10000;i++)
+//		{
+//             System.out.println(i);
+//		   
+//		}
+		
+//		for (int i=1;i<=10;i++)
+//		{
+//			System.out.println("29 * "+i+"="+(i*29));
+//		}
+		//Even - divisible by 2 = reminder =0 
+		//2 , 4 , 6 , 8 , 10 
+//		for(int i=1;i<=100;i++)
+//		{  //3 % 2 1 == 1
+//			if(i % 2 ==1)
+//			{
+//				System.out.println(i);
+//
+//			}
+//		}
+		//1 + 2 + 3+ 4+ 5.....+100
+		// 1 to 100 SUM
+//		int sum=0;
+//		for(int i=1;i<=10;i++)
+//		{
+//			sum = sum + i;
+//		}
+//		
+//		System.out.println("Sum is  : "+sum);
+		
+		//2 +  4 + 6 + 8 + 10
+		// 1  + 3 + 5 + 7 +9
+//		int sum=0;
+//		for(int i=1;i<=10;i++)
+//		{
+//			if(i % 2 == 1)
+//			{
+//				sum = sum + i;
+//
+//			}
+//		}
+		
+		//System.out.println(sum);
+		
+		//Infinite Loop = The Loop is never End
+//		for(;;)
+//		{
+//			System.out.println("Ram");
+//		}
+		
+//		for(int i=1;;)
+//		{
+//			System.out.println("Ram");
+//		}
+		
+//		for(int i=1;i<=10;)
+//		{
+//			System.out.println("Ram");
+//		}
+//		for(int i=1;;i++)
+//		{
+//			System.out.println("Ram");
+//		}
+//		int i=1;
+//		for(;i<=10;)
+//		{
+//			System.out.println("Ram");
+//		}
+		
+		//10 Times it will iterate
+//		int i=1;
+//		for(;i<=10;)
+//		{
+//			System.out.println("Ram");
+//			i++;
+//		}
+		
+//		//Infinite
+//		for(int i=1,j=1;j<=10;i++)
+//		{
+//			System.out.println(j);//1
+//		}
+		
+		
+//		for(int i=1,j=1;i<=10;i++,j++)
+//		{
+//			System.out.println(i +" " +j);
+//		}
+		
+		
+//		for(int i=1,j=2;i<=10;i++,j++)
+//		{
+//			System.out.println(i*j);
+//		}
+		
+		
+//		for(int i=1;i<=5;i++)
+//		{
+//			for(int j=1;j<=5;j++)
+//			{
+//				if(i==3)
+//				{
+//					System.out.print("# ");
+//				}
+//				else if(i==5)
+//				{
+//					System.out.print("$ ");
+//				}
+//				else if(i==1)
+//				{
+//					System.out.print("@ ");
+//				}
+//				else
+//				{
+//					System.out.print("* ");
+//
+//				}
+//			}
+//			System.out.println();
+//		}
+//		
+//		//i = 1   1<=5 true -> 
+		// j=1    1<=5 true -> i =1
+		
+		//Assignment
+//		for(int i=1;i<=5;i++)
+//		{
+//			for(int j=1;j<=5;j++)
+//			{
+//				if(j==1)
+//				{
+//					System.out.print("*");
+//				}
+//				else if(j==3)
+//				{
+//					System.out.print("#");
+//				}
+//				else if(j==5)
+//				{
+//					System.out.print("$");
+//				}
+//				else
+//				{
+//					System.out.print(i); //1 1 1 1 1  //2 2 2 2 2  // 3 3 3 3 3  4 4 4 4 4  5 5 5 5 5
+//
+//				}
+//			}
+//			System.out.println();
+//		}
+//		
+		
+//		for(int i=1;i<=5;i++)
+//		{
+//			for(int j=1;j<=5;j++)
+//			{
+//				if(j==1)
+//				{
+//					System.out.print("*");
+//				}
+		    
+//				else if(j==3)
+//				{
+//					System.out.print("#");
+//				}
+//				else if(j==5)
+//				{
+//					System.out.print("$");
+//				}
+//				else
+//				{
+//					System.out.print(i+1); //1 1 1 1 1  //2 2 2 2 2  // 3 3 3 3 3  4 4 4 4 4  5 5 5 5 5
+//
+//				}
+//			}
+//			System.out.println();
+//		}
+//		
+//		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
